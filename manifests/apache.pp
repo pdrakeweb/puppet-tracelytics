c4e715cf098221abf1cdc53b11f10a68cae417bd
Peter Drake <pdrake@allplayers.com>
2012-07-11 16:00:59 -0400

    Fixed apache settings.

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -1,9 +1,9 @@
-class tracelytics::apache($tracing_mode = "always", $sampling_rate = 300000) {
+class tracelytics::apache($apache_tracing_mode = "always", $apache_sampling_rate = 300000) {
 
   include tracelytics
 
-  $tracing_mode = hiera('tracelytics_tracing_mode', $tracing_mode)
-  $sampling_rate = hiera('tracelytics_sampling_rate', $sampling_rate)
+  $tracing_mode = hiera('tracelytics_tracing_mode', $apache_tracing_mode)
+  $sampling_rate = hiera('tracelytics_sampling_rate', $apache_sampling_rate)
 
   package { "libapache2-mod-oboe":
     ensure  => installed,
